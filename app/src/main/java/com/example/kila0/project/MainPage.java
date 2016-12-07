ac472062b3b2af26b8782e6df8932ed8b175ad48
HuangHongbin <13530517828@163.com>
2016-12-07 14:50:29 +0800

    MainPage Layout -2016.12.7 Hongbin

--- app/src/main/java/com/example/kila0/project/MainPage.java
+++ app/src/main/java/com/example/kila0/project/MainPage.java
@@ -8,5 +8,7 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+
     }
 }
