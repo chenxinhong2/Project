e51bbe00a26de07b826b8a19f1864e4c09b912d9
HuangHongbin <13530517828@163.com>
2016-12-09 12:11:51 +0800

    MainPage Layout -2016.12.9 Hongbin

--- app/src/main/java/com/example/kila0/project/MainPage.java
+++ app/src/main/java/com/example/kila0/project/MainPage.java
@@ -14,7 +14,7 @@ public class MainPage extends AppCompatActivity {
         TabLayout.Tab tab1 = tabLayout.newTab().setText("类别");
         tabLayout.addTab(tab1);
         TabLayout.Tab tab2 = tabLayout.newTab().setText("主页");
-        tabLayout.addTab(tab2,2,true);
+        tabLayout.addTab(tab2);
         TabLayout.Tab tab3 = tabLayout.newTab().setText("添加");
         tabLayout.addTab(tab3);
     }
