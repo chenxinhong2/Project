6d6eff73b9d2483aa49d7ab4e81498ebd2b1e252
bob <413978273@qq.com>
2016-12-10 15:32:06 +0800

    zyb_2016年12月10日15:31:48_2个layout

--- app/src/main/java/com/example/kila0/project/MainPage.java
+++ app/src/main/java/com/example/kila0/project/MainPage.java
@@ -8,14 +8,14 @@ public class MainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_showtask_page);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+      /*  TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         TabLayout.Tab tab1 = tabLayout.newTab().setText("类别");
         tabLayout.addTab(tab1);
         TabLayout.Tab tab2 = tabLayout.newTab().setText("主页");
         tabLayout.addTab(tab2,2,true);
         TabLayout.Tab tab3 = tabLayout.newTab().setText("添加");
-        tabLayout.addTab(tab3);
+        tabLayout.addTab(tab3);*/
     }
 }
